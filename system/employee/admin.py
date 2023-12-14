3289784157dc43aab985a69ffc2a4bde869a23a1
MKurziankou <MKurzenkov.otd14@gmail.com>
2023-12-14 17:01:40 +0300

    Add Employee model and admin panel

--- system/employee/admin.py
+++ system/employee/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'position', 'manager', 'salary', 'salary_info')
+    list_filter = ('position', 'hierarchy_level')
+    actions = ['delete_salary_info']
+
+    @admin.action(description='Delete all information about the paid salary')
+    def delete_salary_info(self, request, queryset):
+        queryset.update(salary_info='')
+
+
+admin.site.register(Employee, EmployeeAdmin)
+
+
+
--- system/employee/models.py
+++ system/employee/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+HIERARCHY_CHOICES = (
+    (0, 'Генеральный директор'),
+    (1, 'Руководитель высшего звена'),
+    (2, 'Менеджер среднего звена'),
+    (3, 'Руководитель отдела'),
+    (4, 'Обычный сотрудник'),
+)
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    date_employment = models.DateField(default=datetime.now, blank=True)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    salary_info = models.CharField(max_length=100)
+    manager = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    hierarchy_level = models.IntegerField(choices=HIERARCHY_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
